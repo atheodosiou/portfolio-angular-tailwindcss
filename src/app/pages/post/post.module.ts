6a79747860b5ff64d5e24736622b594a06b19ca9
Anastasios Theodosiou <anastasios.theodosiou@gmail.com>
2021-10-09 23:55:20 +0300

    Changes

--- src/app/pages/post/post.module.ts
+++ src/app/pages/post/post.module.ts
@@ -2,8 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostComponent } from './post.component';
 import { RouterModule } from '@angular/router';
-import { MarkdownModule, MarkedOptions } from 'ngx-markdown';
+import { MarkdownModule, MarkedOptions, MarkedRenderer } from 'ngx-markdown';
 import { HttpClient } from '@angular/common/http';
+
+// function that returns `MarkedOptions` with renderer override
+export function markedOptionsFactory(): MarkedOptions {
+  const renderer = new MarkedRenderer();
+
+  renderer.heading = (text: string, level: 1 | 2 | 3 | 4 | 5 | 6, raw: string, slugger: marked.Slugger) => {
+    return `<h${level}>${text}</h${level}>`;
+  };
+
+  return {
+    renderer: renderer,
+    gfm: true,
+    breaks: true,
+    pedantic: false,
+    smartLists: true,
+    smartypants: false,
+  };
+}
 
 @NgModule({
   imports: [
@@ -17,13 +35,7 @@ import { HttpClient } from '@angular/common/http';
       loader: HttpClient, // optional, only if you use [src] attribute
       markedOptions: {
         provide: MarkedOptions,
-        useValue: {
-          gfm: true,
-          breaks: false,
-          pedantic: false,
-          smartLists: true,
-          smartypants: false
-        },
+        useValue: markedOptionsFactory
       },
     })
   ],
