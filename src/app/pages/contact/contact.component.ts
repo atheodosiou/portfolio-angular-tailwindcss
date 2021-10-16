0534be8ea9a1e1b79a5b6cfd497812918abfd565
Anastasios Theodosiou <anastasios.theodosiou@gmail.com>
2021-10-16 17:12:06 +0300

    Fixes

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,19 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { SocialProfile } from 'src/app/shared/components/social-icons/social-icons.model';
-import { SocialIconsService } from 'src/app/shared/services/social-icons.service';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent implements OnInit {
-
-  settings: SocialProfile[] = [];
-  constructor(private socialIconsService: SocialIconsService) { }
-
-  ngOnInit() {
-    this.settings = this.socialIconsService.userSocialProfiles;
-  }
-
+export class ContactComponent {
+  constructor() { }
 }
